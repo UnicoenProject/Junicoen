9e62c1f9be14d36bd6a4b71d5f70f7afffafcf78
irxground <irxnjhtchlnrw@gmail.com>
2014-09-20 14:05:20 +0900

    Add eclipse project

--- step1/rakefile.rb
+++ step1/rakefile.rb
@@ -2,7 +2,7 @@ task :javac do
   src_dir = "src_java"
   dst_dir = "dst_java"
 
-  files = Dir.glob("#{src_dir}/*.java")
+  files = Dir.glob("#{src_dir}/**/*.java")
   mkdir_p dst_dir
   sh "pwd"
   sh "javac", "-sourcepath", src_dir, "-d", dst_dir, *files
